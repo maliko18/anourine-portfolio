b86ced0b7c978f00c00a20c47e025f4a65479579
anourine <anourine@innovqube.com>
2025-07-10 01:29:33 +0200

    more personal modification - version 1.4

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -20,7 +20,7 @@ const Photo = () => {
             opacity: 1,
             transition: { delay: 2.4, duration: 0.4, ease: "easeInOut" },
           }}
-          className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute right-0 "
+          className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute xl:right-0 "
         >
           <Image
             src="/assets/photo2.png"
