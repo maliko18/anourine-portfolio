d48e30415af1d9a2619418051c0220bdd9196254
anourine <anourine@innovqube.com>
2025-09-19 08:51:48 +0200

    correct first error with sonarcloud

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -95,7 +95,7 @@ const Contact = () => {
             <ul className="flex flex-col gap-10">
               {info.map((item, index) => {
                 return (
-                  <li key={index} className="flex items-center gap-6 ">
+                  <li key={item.title} className="flex items-center gap-6 ">
                     <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center ">
                       <div className="text-[20px]">{item.icon}</div>
                     </div>
